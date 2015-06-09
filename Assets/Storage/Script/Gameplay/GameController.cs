d91c7eef6f763771d2bf09bb498a53387f5ec92e
EmanuelUmbelino <emanuel.umbelino@hotmail.com>
2015-06-09 18:11:05 -0300

    Version Alpha 0.45
    Persons (Go To School)

---
+++ Assets/Storage/Script/Gameplay/GameController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    public int people;
+    public static int donePeople;
+	void Start () {
+        donePeople = 0;
+	}
+	
+	void Update () {
+        if (donePeople.Equals(people))
+            print("YOU WIN");
+	}
+}
--- Assets/Storage/Script/Gameplay/PersonBehaviour.cs
+++ Assets/Storage/Script/Gameplay/PersonBehaviour.cs
@@ -7,8 +7,14 @@ public class PersonBehaviour : MonoBehaviour {
     void Start()
     {
         moveState = "Go";
-	
 	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag.Equals("Finish"))
+        {
+            GameController.donePeople += 1;
+        }
+    }
     void OnTriggerStay2D(Collider2D col)
     {
         if (col.tag.Equals("Light"))
@@ -19,6 +25,21 @@ public class PersonBehaviour : MonoBehaviour {
             else
                 moveState = "DontGo";
         }
+        if (col.tag.Equals("Up"))
+            transform.eulerAngles = new Vector3(0,0,0);
+        else if (col.tag.Equals("Down"))
+            transform.eulerAngles = new Vector3(0, 0, 180);
+        else if (col.tag.Equals("Left"))
+            transform.eulerAngles = new Vector3(0, 0, 90);
+        else if (col.tag.Equals("Right"))
+            transform.eulerAngles = new Vector3(0, 0, 270);
+        else if(col.tag.Equals("Player"))
+                moveState = "DontGo";
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag.Equals("Player"))
+            moveState = "Go";
     }
     void Move()
     {
